67738a0fafea1bd8e32eef53e8211d86a0e565fc
Ian Eure <ian.eure@gmail.com>
2013-01-31 09:30:50 -0800

    Headers etc

--- vc-browse.el
+++ vc-browse.el
@@ -1,3 +1,29 @@
+;;; vc-browse.el --- Browse to the current file on GitHub
+
+;; Copyright (C) 2013  Ian Eure
+
+;; Author: Ian Eure <ian.eure@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
 (require 'vc-git)
 
 (defun vc-git-transmute-github (remote branch)
@@ -5,7 +31,7 @@
            "^.*github\\.com."
            "https://github.com/"
            (replace-regexp-in-string "\\.git$" "" remote))
-          "/tree/" branch "/" (vc-git-file-path) "#L" (number-to-string 
+          "/tree/" branch "/" (vc-git-file-path) "#L" (number-to-string
                                                        (line-number-at-pos))))
 
 (defconst vc-git-transmutations
@@ -53,3 +79,4 @@
   (kill-new (vc-git-browse-url)))
 
 (provide 'vc-browse)
+;;; vc-browse.el ends here
